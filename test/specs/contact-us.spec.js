ff980acf2eb378af65198c0cb196b208f73062ff
Vitaliy Petryshyn <petryshyn.v@gmail.com>
2023-12-16 20:12:01 +0100

    add basePage and contuctUs page

--- test/specs/contact-us.spec.js
+++ test/specs/contact-us.spec.js
@@ -1,31 +1,21 @@
+import ContactUsPage from "../pageobjects/webdriver-university/contact-us.page"
 describe('webdriveruniversity - contact page', () => {
     beforeEach(async() => {
         await browser.maximizeWindow();
+        await ContactUsPage.open();
     });
     it('valid submission - submit all information', async() => {
-        await browser.url('/Contact-Us/contactus.html');
-        const firstName = await $('//*[@name="first_name"]');
-        const lastName = await $('//*[@name="last_name"]');
-        const emailAdress = await $('//*[@name="email"]');
-        const message = await $('//*[@name="message"]');
-        const submitButton = await $('//input[@value="SUBMIT"]');
-
-        await firstName.setValue('Vital');
-        await lastName.setValue('Petryh');
-        await emailAdress.setValue('Petryh@asd.dsf');
-        await message.setValue('Some message');
-        await submitButton.click();
-        
+        await ContactUsPage.submitForm('Vital', 'Petryh','Petryh@asd.dsf', 'Some message');
+        await ContactUsPage.clickOnSubmitButton();
         const succesfullSubmisionHeader = await $('#contact_reply>h1');
         await expect(succesfullSubmisionHeader).toHaveText('Thank You for your Message!');
-
-        await browser.pause(5000);
     });
 
-    it('invalid submission - dont submmit all info', () => {
-             //first name
-        //last  name
-        //message
-        //submit button
+    it('invalid submission - dont submmit all info', async () => {
+        await ContactUsPage.submitForm('Vital', 'Petryh','', 'Some message');
+        await ContactUsPage.clickOnSubmitButton();
+        const succesfullSubmisionHeader = await $('//body');
+        await expect(succesfullSubmisionHeader).toHaveTextContaining('Error: all fields are required');
+
     });
 });
